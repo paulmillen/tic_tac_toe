a4fcea391e922733bd4eeda5cd49ed447f6a5ff2
paulmillen <pwmillen@gmail.com>
2017-09-06 12:44:14 +0100

    added #claim_field, #turn_over now private

--- lib/game.rb
+++ lib/game.rb
@@ -1,10 +1,27 @@
 class Game
 
-  attr_reader :player_one, :player_two
+  attr_reader :player_X, :player_O, :current_turn
+  attr_accessor :board
 
   def initialize
-    @player_one = true
-    @player_two = true
+    @player_X = 'X'
+    @player_O = 'O'
+    @current_turn = @player_X
+    @board = [  [1,1,1],
+                [1,1,1],
+                [1,1,1]  ];
+  end
+
+  def claim_field(row, column, player)
+    fail 'field occupied' unless @board[row][column] === 1
+    @board[row][column] = player
+    turn_over
+  end
+
+  private
+
+  def turn_over
+    @current_turn === @player_X ? @current_turn = @player_O : @current_turn = @player_X
   end
 
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -2,15 +2,39 @@ describe Game do
 
   subject(:game) { described_class.new }
 
-  describe 'player one' do
-    it 'exists' do
-      expect(game.player_one).not_to be_nil
+  describe '#player_X' do
+    it 'returns X' do
+      expect(game.player_X).to eq 'X'
     end
   end
 
-  describe 'player two' do
-    it 'exists' do
-      expect(game.player_two).not_to be_nil
+  describe '#player_O' do
+    it 'returns O' do
+      expect(game.player_O).to eq 'O'
+    end
+  end
+
+  describe '#current_turn' do
+    it "returns the player whose current turn it is" do
+      expect(game.current_turn).to eq 'X'
+    end
+  end
+
+  describe '#board' do
+    it 'is an empty TicTacToe board upon instantiation' do
+      expect(game.board).to eq [[1, 1, 1], [1, 1, 1], [1, 1, 1]]
+    end
+  end
+
+  describe '#claim_field' do
+    it 'allows a player to claim an empty field' do
+      game.claim_field(1,1,game.player_X)
+      expect(game.board).to eq [[1, 1, 1], [1, 'X', 1], [1, 1, 1]]
+    end
+
+    it 'raises an error if the field is not empty' do
+      game.claim_field(1,1,game.player_X)
+      expect { game.claim_field(1,1,game.player_O) }.to raise_error 'field occupied'
     end
   end
 
